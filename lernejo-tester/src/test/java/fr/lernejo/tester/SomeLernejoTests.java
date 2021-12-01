ae50bde1b381548d9b8914a75f86a3a53b28652c
Costiou Rozenn <rozenn.costiou@orange.fr>
2021-12-01 15:28:14 +0100

    ajout de test

---
+++ lernejo-tester/src/test/java/fr/lernejo/tester/SomeLernejoTests.java
@@ -0,0 +1,17 @@
+package fr.lernejo.tester;
+
+import fr.lernejo.tester.api.TestMethod;
+
+public class SomeLernejoTests {
+    @TestMethod
+    public void ok(){
+
+    }
+    @TestMethod
+    public void ko(){
+        throw new IllegalStateException();
+    }
+    public void none(){
+
+    }
+}
